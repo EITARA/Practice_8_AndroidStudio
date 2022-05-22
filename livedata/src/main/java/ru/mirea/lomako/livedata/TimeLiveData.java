b34e21b92d900855632311abc4ccad7c2dabc435
EITARA <kiluna25@gmail.com>
2022-05-22 23:16:47 +0300

    Initial commit

---
+++ livedata/src/main/java/ru/mirea/lomako/livedata/TimeLiveData.java
@@ -0,0 +1,23 @@
+package ru.mirea.lomako.livedata;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.Date;
+
+class TimeLiveData {
+    private static String TAG;
+    private static MutableLiveData<Long> data = new MutableLiveData<Long>();
+    //sets latest time to LiveData
+    static LiveData<Long> getTime(){
+        data.setValue(new Date().getTime());
+        Log.d(TAG, "get time");
+        return data;
+    }
+    static void setTime(){
+        data.setValue(new Date().getTime());
+        Log.d(TAG, "set time");
+    }
+}
